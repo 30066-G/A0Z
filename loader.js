2ddfa4b4ed0c94aca310839500d86dad11e661c4
30066 <201962374+30066-G@users.noreply.github.com>
2026-06-15 20:04:21 +0200

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,22 @@
+window.addEventListener(
+    "load",
+    () => {
+
+        const loader =
+            document.getElementById(
+                "pageLoader"
+            );
+
+        setTimeout(
+            () => {
+
+                loader?.classList.add(
+                    "hidden"
+                );
+
+            },
+            800
+        );
+
+    }
+);
