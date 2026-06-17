87ca64f3afd449174bca694635ac3c8f371fe269
30066 <201962374+30066-G@users.noreply.github.com>
2026-06-17 17:03:08 +0200

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -399,19 +399,3 @@ if (closeContact && contactModal) {
         contactModal.classList.remove("active");
     });
 }
-particlesJS("particles-js", {
-    particles: {
-        number: { value:200, density:{ enable:true, value_area:1800 } },
-        color: { value:"#ffffff" },
-        shape: { type:"circle" },
-        opacity: { value:0.7, random:true },
-        size: { value:1.5, random:true },
-        line_linked: { enable:false },
-        move: { enable:true, speed:0.5, random:true, straight:false, out_mode:"out" }
-    },
-    interactivity: {
-        detect_on:"canvas",
-        events:{ onhover:{ enable:false }, onclick:{ enable:false } }
-    },
-    retina_detect:true
-});
