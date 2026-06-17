c361c566f211d3e4e958f600b671ee9265b0dc42
30066 <201962374+30066-G@users.noreply.github.com>
2026-06-17 19:44:05 +0200

    Update availability.js

--- availability.js
+++ availability.js
@@ -1,26 +1,30 @@
-const statusBar =
-    document.querySelector(
-        ".status-bar span:last-child"
-    );
+import { db } from "./firebase.js"; 
+import { ref, onValue } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
-const hour =
-    new Date().getHours();
+const statusBar = document.querySelector(".status-bar span:last-child");
+const statusDot = document.querySelector(".status-dot");
 
-if (
-    hour >= 22 ||
-    hour < 4
-) {
+const ordersRef = ref(db, 'orders');
 
-    statusBar.innerText =
-        "Online Nu • Gemiddelde levertijd: 17 min";
+onValue(ordersRef, (snapshot) => {
+    const orders = snapshot.val();
+    
+    const activeOrders = orders ? Object.keys(orders).length : 0;
+    
+    const hour = new Date().getHours();
 
-} else {
+    if (hour >= 21 || hour < 3) {
+        
+        if (activeOrders > 3) {
+            statusDot.style.background = "#f59e0b";
+            statusBar.innerText = `Online Nu • Druk (vertraging 25-40 min) • ${activeOrders} bestellingen`;
+        } else {
+            statusDot.style.background = "#22c55e";
+            statusBar.innerText = "Online Nu • Gemiddelde levertijd: 17 min";
+        }
 
-    document.querySelector(
-        ".status-dot"
-    ).style.background =
-        "#ef4444";
-
-    statusBar.innerText =
-        "Gesloten • Momenteel niet beschikbaar";
-}
+    } else {
+        statusDot.style.background = "#ef4444"; 
+        statusBar.innerText = "Gesloten • Momenteel niet beschikbaar";
+    }
+});
